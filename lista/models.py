64bb1d5b8e13c7b6d028661bfa16d59e48b53515
Kauã Carlos <kauagoldz@gmail.com>
2023-10-25 19:48:24 -0300

    Feat: criado o usuario admin

--- lista/models.py
+++ lista/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class item(models.Model):
+    item = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.item
 # Create your models here.
