da266d23e28782bed13283c50fd7226f4cd6fb34
Kauã Carlos <kauagoldz@gmail.com>
2023-11-01 21:25:27 -0300

    feat: criado o butão de add intem

--- lista/urls.py
+++ lista/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import lista
+from . import views
 
 
 urlpatterns = [
-    path('', lista, name='lista')
+    path('',views.lista, name='lista'),
+    path('add_item', views.add_item, name='add_item')
 ]
