0d0022023a9b46c0046f5be381c5d2666382a686
Kauã Carlos <kauagoldz@gmail.com>
2023-12-11 13:26:51 -0300

    feat:personalização das telas

--- lista/models.py
+++ lista/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Item(models.Model):
     item = models.CharField(max_length=200)
     quantidade = models.IntegerField(default=0)
+    preco = models.FloatField(default=0, null=True)
 
     def __str__(self):
         return self.item
--- lista/views.py
+++ lista/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Item
 from .forms import ItemForm
-
 def lista(request):
     itens = Item.objects.all()
     context = {'itens': itens}
@@ -13,5 +12,16 @@ def add_item(request):
         context = {'form' : form}
         return render(request, "add_item.html", context)
    
-
+def salvar_item(request):
+    allTasks = Item.objects.all()
+    context ={'sucesso': False, 'item':allTasks}
+    if request.method == 'POST':
+        item = request.POST.get['item']
+        quantidade =request.POST.get['quantidade']
+        preco =request.POST.get['preco']
+        item = Item(item = item,quantidade= quantidade,preco=preco)
+        item.save()
+        context ={'sucesso': True,'item': allTasks}
+    context={'item' : allTasks}
+    return render(request, 'index.html', context)
 
